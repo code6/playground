ccaf6fee4d9d276a0fb8f62fa5923703ab1f1184
code6 <wzc1989@gmail.com>
2013-07-21 00:41:56 +0800

    acm code template

---
+++ acm/template/a.cpp
@@ -0,0 +1,49 @@
+/* --- Author: Vladimir Smykalov, enot.1.10@gmail.com --- */
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <ctime>
+#include <cassert>
+
+using namespace std;
+
+#define fs first
+#define sc second
+#define pb push_back
+#define mp make_pair
+#define range(i, n) for (int i=0; i<(n); ++i)
+#define forit(it,v) for(typeof((v).begin()) it = v.begin() ; it != (v).end() ; ++it)
+#define eprintf(...) fprintf(stderr, __VA_ARGS__),fflush(stderr)
+#define sz(a) ((int)(a).size())
+#define all(a) (a).begin(),a.end()
+
+typedef long long ll;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+int main() {
+    vector<PII> vp;
+    vp.pb(mp(5, 2));
+    vp.pb(mp(3, 4));
+    vp.pb(mp(6, 1));
+    range(i, vp.size()) {
+        eprintf("(%d, %d)\n", vp[i].fs, vp[i].sc);
+    }
+    forit(it, vp) {
+        eprintf("(%d, %d)\n", it->fs, it->sc);
+    }
+    cout << "sz = " << sz(vp) << endl;
+    sort(all(vp));
+    forit(it, vp) {
+        eprintf("(%d, %d)\n", it->fs, it->sc);
+    }
+
+    return 0;
+}
