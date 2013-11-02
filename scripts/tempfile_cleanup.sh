2eb7e49c976cd4f68a336ce649eaa95857d5cadd
code6 <wzc1989@gmail.com>
2013-11-02 19:30:06 +0800

    fix tempfile_cleanup.sh

--- scripts/tempfile_cleanup.sh
+++ scripts/tempfile_cleanup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -e
-# Ref: http://stackoverflow.com/questions/3469705/how-to-run-script-commands-from-variables
-#
+# Ref:
+# http://stackoverflow.com/questions/3469705/how-to-run-script-commands-from-variables
+# http://superuser.com/questions/375011/bash-script-to-delete-files-older-than-x-days-with-subdirectories
+# http://superuser.com/questions/152958/exclude-hidden-files-when-searching-with-unix-linux-find
+# http://stackoverflow.com/questions/13525004/how-to-exclude-this-current-dot-folder-from-find-type-d
 
 usage() {
 cat << EOF
@@ -9,9 +12,11 @@ This script use to clean up tempfile/dir
 
 options:
     --dir                   cleanup dir(required)
+    -t, --type              clean type f -- file , d -- dir, default are both
     --delta                 delta days
+    --delta-min             delta minutes
     -k, --keyword           keyword
-    -r, --recursive         recursive clean up tempfile, otherwise only clean files with depth=1
+    -r, --recursive         default will cleanup file/dir with maxdepth = 1, using -r one can recursive delete files with '-t f' together
     -f, --force             force delete without confirm
     -v, --verbose           show all deleted files/dir if verbose is on
     --dry-run               just dump conf, no run
@@ -24,7 +29,8 @@ dump_conf() {
 cat << EOF
 conf = 
     DIR=$DIR
-    DELTA=$DELTA
+    TYPE=$TYPE
+    DELTA_MINUTE=$DELTA_MINUTE
     KEYWORD=$KEYWORD
     RECURSIVE=$RECURSIVE
     VERBOSE=$VERBOSE
@@ -38,9 +44,10 @@ EOF
 }
 
 DIR=
-DELTA=30
+TYPE=
+DELTA_MINUTE=$((24*30*60))
 KEYWORD=""
-RECURSIVE="-maxdepth 1 -type f"
+RECURSIVE="-maxdepth 1"
 VERBOSE=
 FORCE=
 DRY_RUN=
@@ -52,9 +59,19 @@ while [ $# -gt 0 ]; do
             DIR=$1
             shift
             ;;
+        --type|-t)
+            shift
+            TYPE=" -type $1 "
+            shift
+            ;;
         --delta)
             shift
-            DELTA=$1
+            DELTA_MINUTE=$(($1*24*60))
+            shift 
+            ;;
+        --delta-min)
+            shift
+            DELTA_MINUTE=$1
             shift 
             ;;
         --recursive|-r)
@@ -79,7 +96,7 @@ while [ $# -gt 0 ]; do
             DRY_RUN=1
             ;;
         --help|-h)
-            help
+            usage
             exit 0
             ;;
         *)
@@ -94,26 +111,30 @@ if [[ -z $DIR ]]; then
     exit 1
 fi
 
-LIST_CMD="cd $DIR && find . $KEYWORD $RECURSIVE -mtime +$DELTA -exec echo '{}' \;"
-CLEAN_CMD="cd $DIR && find . $KEYWORD $RECURSIVE -mtime +$DELTA -delete;"
+LIST_CMD="cd $DIR && find . $RECURSIVE \( ! -name '.*' \) $KEYWORD -mmin +$DELTA_MINUTE $TYPE -exec echo '{}' \;"
+CLEAN_CMD="cd $DIR && find . $RECURSIVE \( ! -name '.*' \) $KEYWORD -mmin +$DELTA_MINUTE $TYPE -exec rm -r '{}' '+'"
+
+echo "tempfile cleanup at $DIR on `date`"
+
+total=$(echo `bash -c "$LIST_CMD | wc -l"`)
+echo "$total files will be deleted."
 
 if [[ $DRY_RUN == 1 ]]; then
     dump_conf
+    echo "dry run, nothing happened"
     exit 0
 fi
+
 
 if [[ $VERBOSE == 1 ]] ; then
     echo " Files to be deleted: "
     bash -c "$LIST_CMD | cat"
-else
-    echo " Files to be deleted(top 50): "
-    bash -c "$LIST_CMD | head -n 50"
 fi
 
 #check 
 if [[ $FORCE != 1 ]]; then
     while true; do
-        read -p "Do you want to delete above files(yes or no) ?" yn
+        read -p "Do you want to delete above $total files(yes or no) ?" yn
         case $yn in
             [Yy]* ) break;;
             [Nn]* ) echo "No files are deleted, exit..."; exit;;
@@ -124,3 +145,4 @@ fi
 
 #apply
 bash -c "$CLEAN_CMD"
+echo "$total files have been deleted"
