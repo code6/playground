560ad3042601192f3f07f42bf90139f87f67759f
code6 <wzc1989@gmail.com>
2014-10-03 20:57:00 +0800

    algo thinking week3

---
+++ courses/algorithmicthink-001/week3/alg_project3.py
@@ -0,0 +1,180 @@
+"""
+Template for Project 3
+Student will implement four functions:
+
+slow_closest_pairs(cluster_list)
+fast_closest_pair(cluster_list) - implement fast_helper()
+hierarchical_clustering(cluster_list, num_clusters)
+kmeans_clustering(cluster_list, num_clusters, num_iterations)
+
+where cluster_list is a list of clusters in the plane
+"""
+
+import math
+import alg_cluster
+
+EPS = 1e-9
+
+
+def pair_distance(cluster_list, idx1, idx2):
+    """
+    Helper function to compute Euclidean distance between two clusters
+    in cluster_list with indices idx1 and idx2
+    
+    Returns tuple (dist, idx1, idx2) with idx1 < idx2 where dist is distance between
+    cluster_list[idx1] and cluster_list[idx2]
+    """
+    return (cluster_list[idx1].distance(cluster_list[idx2]), min(idx1, idx2), max(idx1, idx2))
+
+
+def slow_closest_pairs(cluster_list):
+    """
+    Compute the set of closest pairs of cluster in list of clusters
+    using O(n^2) all pairs algorithm
+    
+    Returns the set of all tuples of the form (dist, idx1, idx2) 
+    where the cluster_list[idx1] and cluster_list[idx2] have minimum distance dist.   
+    
+    """
+    
+    min_dist = -1.0
+    min_list = []
+    cluster_size = len(cluster_list)
+    for idx1 in range(cluster_size):
+        for idx2 in range(cluster_size):
+            if idx1 == idx2:
+                continue
+            dist_tuple = pair_distance(cluster_list, idx1, idx2)
+            dist = dist_tuple[0]
+            if min_dist < 0 or dist < min_dist - EPS:
+                min_dist = dist
+                min_list = [dist_tuple]
+            elif abs(dist - min_dist) < EPS:
+                min_list.append(dist_tuple)
+    return set(min_list)
+
+
+def fast_closest_pair(cluster_list):
+    """
+    Compute a closest pair of clusters in cluster_list
+    using O(n log(n)) divide and conquer algorithm
+    
+    Returns a tuple (distance, idx1, idx2) with idx1 < idx 2 where
+    cluster_list[idx1] and cluster_list[idx2]
+    have the smallest distance dist of any pair of clusters
+    """
+        
+    def fast_helper(cluster_list, horiz_order, vert_order):
+        """
+        Divide and conquer method for computing distance between closest pair of points
+        Running time is O(n * log(n))
+        
+        horiz_order and vert_order are lists of indices for clusters
+        ordered horizontally and vertically
+        
+        Returns a tuple (distance, idx1, idx2) with idx1 < idx 2 where
+        cluster_list[idx1] and cluster_list[idx2]
+        have the smallest distance dist of any pair of clusters
+    
+        """
+        num = len(horiz_order)
+        
+        # base case
+        if num <= 1:
+            return (1e30, 0, 0)
+        if num <= 3:
+            result =  list(slow_closest_pairs([cluster_list[pos] for pos in horiz_order]))[0]
+            #mapping cluster index
+            result = (result[0], horiz_order[result[1]], horiz_order[result[2]])
+            return result
+        
+        # divide
+        half = num / 2
+        horiz_left_index = horiz_order[0:half]
+        horiz_right_index = horiz_order[half:num]
+        vert_left_index = [pos for pos in vert_order if pos in horiz_left_index]
+        vert_right_index = [pos for pos in vert_order if pos in horiz_right_index]
+        dist_tuple_left = fast_helper(cluster_list, horiz_left_index, vert_left_index)
+        dist_tuple_right = fast_helper(cluster_list, horiz_right_index, vert_right_index)
+        
+        # conquer
+        dist_tuple = min(dist_tuple_left, dist_tuple_right)
+        horiz_mid_val = 0.5 * (cluster_list[horiz_order[half - 1]].horiz_center() + cluster_list[horiz_order[half]].horiz_center())
+
+        check_pos_list = [ pos for pos in vert_order if abs(cluster_list[pos].horiz_center() - horiz_mid_val) < dist_tuple[0]]
+        check_num = len(check_pos_list)
+        for idx_u in range(check_num):
+            for idx_v in range(idx_u + 1, min(idx_u + 3, check_num - 1) + 1):
+                dist_tuple = min(dist_tuple, pair_distance(cluster_list, check_pos_list[idx_u], check_pos_list[idx_v]))
+        #print "dist_tuple = ", dist_tuple
+        return dist_tuple
+            
+    # compute list of indices for the clusters ordered in the horizontal direction
+    hcoord_and_index = [(cluster_list[idx].horiz_center(), idx) 
+                        for idx in range(len(cluster_list))]    
+    hcoord_and_index.sort()
+    horiz_order = [hcoord_and_index[idx][1] for idx in range(len(hcoord_and_index))]
+     
+    # compute list of indices for the clusters ordered in vertical direction
+    vcoord_and_index = [(cluster_list[idx].vert_center(), idx) 
+                        for idx in range(len(cluster_list))]    
+    vcoord_and_index.sort()
+    vert_order = [vcoord_and_index[idx][1] for idx in range(len(vcoord_and_index))]
+
+    # compute answer recursively
+    answer = fast_helper(cluster_list, horiz_order, vert_order) 
+    return (answer[0], min(answer[1:]), max(answer[1:]))
+
+    
+
+def hierarchical_clustering(cluster_list, num_clusters):
+    """
+    Compute a hierarchical clustering of a set of clusters
+    Note: the function mutates cluster_list
+    
+    Input: List of clusters, number of clusters
+    Output: List of clusters whose length is num_clusters
+    """
+    while len(cluster_list) > num_clusters:
+        #dist_tuple = list(slow_closest_pairs(cluster_list))[0]
+        dist_tuple = fast_closest_pair(cluster_list)
+        idx1, idx2 = dist_tuple[1:]
+        cluster_list[idx1].merge_clusters(cluster_list[idx2])
+        cluster_list.pop(idx2)
+    return cluster_list
+    
+def kmeans_clustering(cluster_list, num_clusters, num_iterations):
+    """
+    Compute the k-means clustering of a set of clusters
+    
+    Input: List of clusters, number of clusters, number of iterations
+    Output: List of clusters whose length is num_clusters
+    """
+    
+    num = len(cluster_list)
+    cluster_index = range(num)
+    cluster_index.sort(key=lambda x:cluster_list[x].total_population())
+    cluster_index.reverse()
+    # initialize k-means clusters to be initial clusters with largest populations
+    center_list = [ cluster_list[cluster_index[pos]].copy() for pos in range(num_clusters) ]
+    prev_center_list = []
+
+    for _ in range(num_iterations):
+        prev_center_list = center_list
+        center_list = []
+        for idx in range(num_clusters):
+            center_list.append(alg_cluster.Cluster(set(), 0.0, 0.0, 0, 0.0))
+        best_idx_list = []
+        for idx in range(num):
+            best_dist = prev_center_list[0].distance(cluster_list[idx])
+            best_idx = 0
+            for cidx in range(num_clusters):
+                dist = prev_center_list[cidx].distance(cluster_list[idx])
+                if best_dist > dist:
+                    best_idx = cidx
+                    best_dist = dist
+            best_idx_list.append(best_idx)
+        for idx in range(num):
+            center_list[best_idx_list[idx]].merge_clusters(cluster_list[idx])
+
+    return center_list
---
+++ courses/algorithmicthink-001/week3/alg_project3_viz.py
@@ -0,0 +1,158 @@
+"""
+Example code for creating and visualizing
+cluster of county-based cancer risk data
+
+Note that you must download the file
+http://www.codeskulptor.org/#alg_clusters_matplotlib.py
+to use the matplotlib version of this code
+"""
+
+# Flavor of Python - desktop or CodeSkulptor
+DESKTOP = True
+
+import math
+import random
+import urllib2
+import alg_cluster
+
+# conditional imports
+if DESKTOP:
+    import alg_project3      # desktop project solution
+    import alg_clusters_matplotlib
+else:
+    #import userXX_XXXXXXXX as alg_project3   # CodeSkulptor project solution
+    import alg_clusters_simplegui
+    import codeskulptor
+    codeskulptor.set_timeout(30)
+
+
+###################################################
+# Code to load data tables
+
+# URLs for cancer risk data tables of various sizes
+# Numbers indicate number of counties in data table
+
+DIRECTORY = "http://commondatastorage.googleapis.com/codeskulptor-assets/"
+DATA_3108_URL = DIRECTORY + "data_clustering/unifiedCancerData_3108.csv"
+DATA_896_URL = DIRECTORY + "data_clustering/unifiedCancerData_896.csv"
+DATA_290_URL = DIRECTORY + "data_clustering/unifiedCancerData_290.csv"
+DATA_111_URL = DIRECTORY + "data_clustering/unifiedCancerData_111.csv"
+
+
+def load_data_from_url(data_url):
+    data_file = urllib2.urlopen(data_url)
+    data = data_file.read()
+    data_lines = data.split('\n')
+    return load_data_table(data_lines)
+
+
+def load_data_from_file(data_file):
+    data_lines = open(data_file, "r").read().split('\n')
+    return load_data_table(data_lines)
+
+def load_data_table(data_lines):
+    """
+    Import a table of county-based cancer risk data
+    from a csv format file
+    """
+    print "Loaded", len(data_lines), "data points"
+    data_tokens = [line.split(',') for line in data_lines]
+    return [[tokens[0], float(tokens[1]), float(tokens[2]), int(tokens[3]), float(tokens[4])] 
+            for tokens in data_tokens]
+
+
+############################################################
+# Code to create sequential clustering
+# Create alphabetical clusters for county data
+
+def sequential_clustering(singleton_list, num_clusters):
+    """
+    Take a data table and create a list of clusters
+    by partitioning the table into clusters based on its ordering
+    
+    Note that method may return num_clusters or num_clusters + 1 final clusters
+    """
+    
+    cluster_list = []
+    cluster_idx = 0
+    total_clusters = len(singleton_list)
+    cluster_size = float(total_clusters)  / num_clusters
+    
+    for cluster_idx in range(len(singleton_list)):
+        new_cluster = singleton_list[cluster_idx]
+        if math.floor(cluster_idx / cluster_size) != \
+           math.floor((cluster_idx - 1) / cluster_size):
+            cluster_list.append(new_cluster)
+        else:
+            cluster_list[-1] = cluster_list[-1].merge_clusters(new_cluster)
+            
+    return cluster_list
+                
+
+#####################################################################
+# Code to load cancer data, compute a clustering and 
+# visualize the results
+
+
+def run_example():
+    """
+    Load a data table, compute a list of clusters and 
+    plot a list of clusters
+
+    Set DESKTOP = True/False to use either matplotlib or simplegui
+    """
+    #data_table = load_data_table(DATA_3108_URL)
+    data_table = load_data_from_file("unifiedCancerData_111.csv")
+    #data_table = load_data_from_file("unifiedCancerData_896.csv")
+    
+    singleton_list = []
+    for line in data_table:
+        singleton_list.append(alg_cluster.Cluster(set([line[0]]), line[1], line[2], line[3], line[4]))
+
+    #print alg_project3.fast_closest_pair(singleton_list)
+    #print alg_project3.slow_closest_pairs(singleton_list)
+        
+    #cluster_list = sequential_clustering(singleton_list, 15)    
+    #print "Displaying", len(cluster_list), "sequential clusters"
+
+    #cluster_list = alg_project3.hierarchical_clustering(singleton_list, 9)
+    #print "Displaying", len(cluster_list), "hierarchical clusters"
+
+    cluster_list = alg_project3.kmeans_clustering(singleton_list, 40, 10)   
+    print "Displaying", len(cluster_list), "k-means clusters"
+    for cluster in cluster_list:
+        print cluster
+
+            
+    #draw the clusters using matplotlib or simplegui
+    #if DESKTOP:
+    #    alg_clusters_matplotlib.plot_clusters(data_table, cluster_list, True)
+    #else:
+    #    alg_clusters_simplegui.PlotClusters(data_table, cluster_list)
+    
+run_example()
+
+
+
+
+
+    
+
+
+
+
+
+  
+        
+
+
+
+
+
+
+        
+
+
+
+
+
